214cef2523775b2308cb3b69e1be133173357efa
michael lynch <nycynik@gmail.com>
2017-02-13 22:52:39 -0500

    chapter 2

---
+++ chapter2/SquareSpiral2.py
@@ -0,0 +1,7 @@
+# SquareSprial1.py
+import turtle
+
+t = turtle.Pen()
+for x in range(100):
+    t.forward(x)
+    t.left(91)
---
+++ chapter2/SquareSpiral3.py
@@ -0,0 +1,11 @@
+# SquareSprial1.py
+import turtle
+
+t = turtle.Pen()
+turtle.bgcolor("black")
+colors = ["red", "green", "yellow", "blue"]
+
+for x in range(100):
+    t.pencolor(colors[x%4])
+    t.forward(x)
+    t.left(91)
